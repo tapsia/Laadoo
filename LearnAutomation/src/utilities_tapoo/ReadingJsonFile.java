d78604fdd216d715bcf70433ce691c2d4df76ce1
tapsia <taps.koul04@gmail.com>
2017-02-11 12:23:17 +0530

    Added classes only

---
+++ LearnAutomation/src/utilities_tapoo/ReadingJsonFile.java
@@ -0,0 +1,10 @@
+package utilities_tapoo;
+
+public class ReadingJsonFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ LearnAutomation/src/utilities_tapoo/ReadingPropertyFile.java
@@ -0,0 +1,10 @@
+package utilities_tapoo;
+
+public class ReadingPropertyFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
