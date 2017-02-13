f635ca7191a4a3966fb0548fdfb13c33cf06618c
Tapsiak <you.email@test.com>
2017-02-13 15:08:30 +0530

    Added csv file and code from ofice

--- LearnAutomation/src/utilities_tapoo/ReadingCsvFile.java
+++ LearnAutomation/src/utilities_tapoo/ReadingCsvFile.java
@@ -1,10 +1,17 @@
 package utilities_tapoo;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
 public class ReadingCsvFile {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	 public static void main(String[] args) throws Exception {
+         String splitBy = ",";
+ BufferedReader br = new BufferedReader(new FileReader("D:\\Laadoo\\LearnAutomation\\src\\utilities_tapoo\\test.csv.txt"));
+ String line = br.readLine();
+ while ((line = br.readLine()) !=null){
+      String[] b = line.split(splitBy);
+      System.out.println(b[0] +"\t" + b[1] +"\t" +b[2] +"\t" +b[3] +"\t" + b[4]+ "\t" + b[5]+ "\t" + b[6]);
+ }
+ br.close();
 
-	}
-
+}
 }
